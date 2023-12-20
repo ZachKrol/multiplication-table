9b7d555f5c17eea76f0f8560c91dcab710e7561c
Zach Krol <61989585+ZachKrol@users.noreply.github.com>
2023-12-20 13:09:23 -0500

    feat: Add multiplication table to main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Enter a number to be the upper bound of a multiplication table: ");
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= num; j++) {
+                System.out.print(i * j);
+            }
+            System.out.println();
+        }
+
     }
 }
